d7c7f178b726ba6398e3ce2b0ca1255ee3dda68d
BineetaSJoseph <josephbineeta89@gmail.com>
2026-07-12 13:50:32 +0530

    Updated ALLOWED_HOSTS for Render

--- gadgethub/settings.py
+++ gadgethub/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ov_-=2z%ert8@sd_%w5s1%^v0@89)saw=rnb)4_wr1%z(ai7so'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
